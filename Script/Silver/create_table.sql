926cce979d15f5a1f004364e489a235dd56cb706
ngthaonguyen0110 <ng.thaonguyen.0110@gmail.com>
2025-11-10 16:45:11 +0700

    Create create_table.sql

---
+++ Script/Silver/create_table.sql
@@ -0,0 +1,82 @@
+/*
+===============================================================================
+Thí script creates tables in 'silver' schema, drops tables if they already exists.
+===============================================================================
+*/
+
+Use [DataWarehouse];
+
+if OBJECT_ID ('silver.crm_cust_info','U') is not null
+	drop table silver.crm_cust_info;
+go
+create table silver.crm_cust_info(
+cst_id int,
+cst_key nvarchar(50),
+cst_firstname nvarchar(50),
+cst_lastname nvarchar(50),
+cst_material_status nvarchar(50),
+cst_gndr nvarchar(50),
+cst_create_date date,
+dwh_create_date datetime2 default GETDATE()
+);
+go
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info;
+go
+create table silver.crm_prd_info(
+prd_id int,
+prd_key nvarchar(50),
+prd_nm nvarchar(50),
+prd_cost int,
+prd_line nvarchar(50),
+prd_start_dt datetime,
+prd_end_dt datetime,
+dwh_create_date datetime2 default GETDATE()
+);
+go
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details;
+go
+create table silver.crm_sales_details(
+sls_ord_num nvarchar(50),
+sls_prd_key nvarchar(50),
+sls_cust_id int,
+sls_order_dt int,
+sls_ship_dt int,
+sls_due_dt int,
+sls_sales int,
+sls_quantity int,
+sls_price int,
+dwh_create_date datetime2 default GETDATE()
+);
+go
+  IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101;
+go
+create table silver.erp_loc_a101(
+cid nvarchar(50),
+cntry nvarchar(50),
+dwh_create_date datetime2 default GETDATE()
+);
+go
+  IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12;
+go
+create table silver.erp_cust_az12(
+cid nvarchar(50),
+bdate date,
+gen nvarchar(50),
+dwh_create_date datetime2 default GETDATE()
+);
+
+go
+  IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2;
+go
+create table silver.erp_px_cat_g1v2 (
+id nvarchar(50),
+cat nvarchar(50),
+subcat nvarchar(50),
+maintenance nvarchar(50),
+dwh_create_date datetime2 default GETDATE()
+);
